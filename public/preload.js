ba978516fd41aa41526e4c42b3319a0fe0bc75e5
Jason <Jason@smultar.com>
2021-11-10 17:46:01 -0500

    Read/Write of `ttmps` file.

--- public/preload.js
+++ public/preload.js
@@ -7,6 +7,26 @@ const { dialog, shell } = require('@electron/remote');
 
 const ADM = require('adm-zip');
 
+/**
+ * Change the compression method of all entries
+ * @param {AdmZip} zip
+ * @param {number} method - Compression method 0 (STORED) or 8 (DEFLATED)
+ * @returns {void}
+ */
+
+ function setGlobalMethod(zip, method) {
+  var entries = zip.getEntries();
+
+  console.log(entries);
+  for (var i = 0; i < entries.length; i++) {
+    var entry = entries[i];
+    entry.header.method = method;
+    console.log(entry);
+  }
+}
+
+
+
 contextBridge.exposeInMainWorld('link', {
   api: {
     on(channel, func) {
@@ -44,15 +64,45 @@ contextBridge.exposeInMainWorld('link', {
       }
 
     },
+    saveFile: async () => {
+      const file = await dialog.showSaveDialog({properties: ['showOverwriteConfirmation', 'createDirectory'], filters: [{ name: 'TTMP', extensions: ['ttmp', 'ttmp2', 'ttmp1'] }]}, (filePaths) => {});
+
+      if (file) return file.filePath;
+
+    },
   },
 
   internal: {
     unpack: (data) => {
       const zip = new ADM(data);
+
       const files = zip.getEntries();
+
       var decompressedData = zip.readAsText(files[0]);
-      //console.log(decompressedData);
+      console.log(decompressedData);
+      console.log(JSON.parse(decompressedData));
+
+
       return decompressedData;
+    },
+
+    export: (origin, file, data) => {
+      console.log('Starting export process');
+      
+      console.log('Loading original file');
+      const originZIP = new ADM(origin); 
+      const originFiles = originZIP.getEntries();
+      const originDecompressedData = originZIP.readFile(originFiles[1]);
+      
+      console.log('Writing Memory File');
+      const zip = new ADM(data); 
+      //zip.addFile('TTMPL.mpl', Buffer.from(JSON.stringify(data), "utf8"));
+      zip.addFile('TTMPL.mpl', Buffer.alloc(JSON.stringify(data).length, JSON.stringify(data))); var entry = zip.getEntry("TTMPL.mpl"); entry.header.method = 0;
+      zip.addFile('TTMPL.mpd', Buffer(originDecompressedData));
+      
+      console.log('Exporting');
+      ///setGlobalMethod(zip, 0);
+      zip.writeZip(file);
     }
   },
   
--- src/comp/window/main.jsx
+++ src/comp/window/main.jsx
@@ -3,6 +3,7 @@ import Select from 'react-select';
 
 function Component({ config, window, history}) {
     const [file, setFile] = useState([]);
+    const [origin, setOrigin] = useState('');
     const [editor, setEditor] = useState({mods: [], content: { name: '', description: 'No description provided, would you like to add one?', author: '', version: ''}, file: { name: '', content: ''}, selected: null});
 
     const [selected, setSelected] = useState({options: [
@@ -20,18 +21,18 @@ function Component({ config, window, history}) {
         
         if (file) {
             let content = window.link.internal.unpack(file);
-            let read = JSON.parse(content);
+            let read = JSON.parse(content); console.log(read);
 
-            setFile(read); 
+            setFile(read); setOrigin(file);
             
             let newEditor = Object.assign({}, editor);
             console.log(editor)
             console.log(newEditor);
             newEditor.mods = separate(read); 
             newEditor.content.name = read.Name; 
-            newEditor.content.description = read.Description ? read.Description : 'No description provided, would you like to add one?';; 
-            newEditor.content.author = read.Author; 
-            newEditor.content.version = read.Version;
+            newEditor.content.description = read?.Description ? read.Description : 'No description provided, would you like to add one?'; 
+            newEditor.content.author = read?.Author ? read.Author : 'No Author';
+            newEditor.content.version = read?.Version ? read.Version : 'Error';
 
             setEditor(newEditor);
         }
@@ -40,20 +41,26 @@ function Component({ config, window, history}) {
     const separate = (target) => {  
         let pairedByName = [];
 
-        target.SimpleModsList.forEach((mod, index) => { 
-            
-            if (pairedByName.filter(currentMod => ((currentMod.name == mod.Name) && (currentMod.category == mod.Category))).length !== 0) {
-                pairedByName.find(currentMod => ((currentMod.name == mod.Name) && (currentMod.category == mod.Category))).mods.push(mod);
+        if (target?.SimpleModsList?.length > 0) {
+            target.SimpleModsList.forEach((mod, index) => { 
                 
-            } else {
-                pairedByName.push({name: mod.Name, category: mod.Category, mods: [mod]});
-            }
+                if (pairedByName.filter(currentMod => ((currentMod.name == mod.Name) && (currentMod.category == mod.Category))).length !== 0) {
+                    pairedByName.find(currentMod => ((currentMod.name == mod.Name) && (currentMod.category == mod.Category))).mods.push(mod);
+                    
+                } else {
+                    pairedByName.push({name: mod.Name, category: mod.Category, mods: [mod]});
+                }
+    
+                if (index == target.SimpleModsList.length - 1) {
+                    console.log(pairedByName);
+                    return pairedByName;
+                }
+            });
 
-            if (index == target.SimpleModsList.length - 1) {
-                console.log(pairedByName);
-                return pairedByName;
-            }
-        });
+        } else {
+            pairedByName.push({name: target.Name, category: target.Category, mods: [target], single: true});
+        }
+
 
         return pairedByName;
     };
@@ -98,6 +105,69 @@ function Component({ config, window, history}) {
         newEditor.mods[editor.selected].name = content;
         setEditor(newEditor);
     }
+
+    const exportPack = async (editor) => {
+        let saveFile = await window.link.action.saveFile();
+
+        let pack = Object.assign({}, file);
+
+        if (editor.mods[0]?.single) {
+            pack.Name = editor.content.name;
+            pack.Description = editor.content.description;
+            pack.Author = editor.content.author;
+            //pack.Version = '1.0.1';
+            
+            pack.Category = editor.mods[0].mods[0].Category;
+            pack.DatFile = editor.mods[0].mods[0].DatFile;
+            pack.ModOffset = editor.mods[0].mods[0].ModOffset;
+            pack.ModSize = editor.mods[0].mods[0].ModSize;
+            pack.FullPath = editor.mods[0].mods[0].FullPath;
+            console.log(pack);
+
+            await window.link.internal.export(origin, saveFile, pack);
+
+
+        } else {
+
+            pack.Name = editor.content.name;
+            pack.Description = editor.content.description;
+            pack.Author = editor.content.author;
+            pack.Version = editor.content.version;
+    
+            let modList = [];
+    
+            editor.mods.forEach((parent, index) => { 
+                
+                console.log(parent);
+                let updatedList = parent.mods.map(child => {
+    
+                    return {
+                        Name: parent.name,
+                        Category: parent.category,
+                        FullPath: child.FullPath,
+                        ModOffset: child.ModOffset,
+                        ModSize: child.ModSize,
+                        ModPackEntry: {name: child.Name, author: editor.content.author, version: editor.content.version, url: ''}
+                    }
+                })
+    
+                modList.push(...updatedList);
+            });
+    
+    
+            console.log(modList)
+    
+            pack.SimpleModsList = modList;
+    
+            console.log(pack);
+            console.log(origin);
+            await window.link.internal.export(origin, saveFile, pack);
+        }
+
+
+    }
+
+    //TTMPD.mpd "TTMPL.mpl"
     
     return (
         <div id="dock-render">
@@ -109,6 +179,8 @@ function Component({ config, window, history}) {
                         <p className="title">TTMP Tweaker</p>
                         <p className="link">Issues</p>
                         <p className="link">Changes</p>
+                        <div className="load-file" onClick={() => {exportPack(editor)}}>Select a file</div>
+
                     </div>
                     <p className="description">Modify <span className="tag">.ttmp</span> files without all the hassle.</p>
                 </div>
